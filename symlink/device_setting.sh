9af54f6a76702f15ed1464f701338e4465b2c304
Junsang Ryu <50894726+js-ryu@users.noreply.github.com>
2020-10-08 23:16:43 +0900

    Update device_setting.sh

--- symlink/device_setting.sh
+++ symlink/device_setting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /etc/udev/rules.d
-sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AK05MK9T", MODE = "0777", SYMLINK+="artivIMU"' > 50-usb-imu.rules
+sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", MODE = "0777", SYMLINK+="artivIMU"' > 50-usb-imu.rules
 
 sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="A50285BI", MODE = "0777", SYMLINK+="artivGPS"' > 51-usb-gps.rules
 
