45658e01caed7c7c11ac08e864bf365dfbb3b6c6
EunbinSeo <53460541+EunbinSeo@users.noreply.github.com>
2020-10-12 06:33:56 +0900

    Update device_setting.sh

--- symlink/device_setting.sh
+++ symlink/device_setting.sh
@@ -5,4 +5,11 @@ sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="067b", ATTRS{idPr
 
 sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="A50285BI", MODE = "0777", SYMLINK+="artivGPS"' > 51-usb-gps.rules
 
+# Camera info for erp42
+sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="082d", KERNEL=="video*", ATTRS{serial}=="3F53FE4F", SYMLINK+="artivObjectCamera"' > 30-usb-ObjectCamera.rules
+sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="0843", KERNEL=="video*", ATTRS{serial}=="B25BDE8E", SYMLINK+="artivParkingCamera"' > 31-usb-ParkingCamera.rules
+
+
+
+
 #udevadm control --reload-rules && udevadm trigger
